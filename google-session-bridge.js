ebbbf2ef5bed872cb2087e885c906d3c47b9d0d5
0xGitteth <160280586+0xGitteth@users.noreply.github.com>
2026-08-20 20:43:54 +0200

    Tighten session bridge lifecycle

--- google-session-bridge.js
+++ google-session-bridge.js
@@ -6,32 +6,56 @@ const Module = require('module');
 
 const SESSION_DECLARATION = 'const sessions = new Map();';
 const SESSION_REPLACEMENT = 'const sessions = globalThis.__BOEKENBAAI_SESSIONS;';
+const GLOBAL_SESSION_KEY = '__BOEKENBAAI_SESSIONS';
 
 function installSessionBridge(options = {}) {
   const sessions = options.sessions || new Map();
   const target = path.resolve(
     options.target || process.env.BOEKENBAAI_SERVER_ENTRY || path.join(__dirname, 'server.js')
   );
-  globalThis.__BOEKENBAAI_SESSIONS = sessions;
+  if (Object.prototype.hasOwnProperty.call(globalThis, GLOBAL_SESSION_KEY)) {
+    throw new Error('Boekenbaai sessiebrug is al geïnstalleerd.');
+  }
+  globalThis[GLOBAL_SESSION_KEY] = sessions;
 
   const originalLoader = Module._extensions['.js'];
   let transformed = false;
+  let restored = false;
+
+  function restoreLoader() {
+    if (!restored && Module._extensions['.js'] !== originalLoader) {
+      Module._extensions['.js'] = originalLoader;
+    }
+    restored = true;
+  }
+
+  function clearGlobalReference() {
+    if (globalThis[GLOBAL_SESSION_KEY] === sessions) {
+      delete globalThis[GLOBAL_SESSION_KEY];
+    }
+  }
 
   Module._extensions['.js'] = function boekenbaaiSessionLoader(module, filename) {
     if (path.resolve(filename) !== target) {
       return originalLoader(module, filename);
     }
 
-    Module._extensions['.js'] = originalLoader;
+    restoreLoader();
     const source = fs.readFileSync(filename, 'utf8');
     const occurrences = source.split(SESSION_DECLARATION).length - 1;
     if (occurrences !== 1) {
+      clearGlobalReference();
       throw new Error(
         `Boekenbaai sessiebrug verwacht exact één sessiedeclaratie, maar vond er ${occurrences}.`
       );
     }
+
     transformed = true;
-    return module._compile(source.replace(SESSION_DECLARATION, SESSION_REPLACEMENT), filename);
+    try {
+      return module._compile(source.replace(SESSION_DECLARATION, SESSION_REPLACEMENT), filename);
+    } finally {
+      clearGlobalReference();
+    }
   };
 
   return {
@@ -41,9 +65,8 @@ function installSessionBridge(options = {}) {
       return transformed;
     },
     restore() {
-      if (Module._extensions['.js'] !== originalLoader) {
-        Module._extensions['.js'] = originalLoader;
-      }
+      restoreLoader();
+      clearGlobalReference();
     },
   };
 }
